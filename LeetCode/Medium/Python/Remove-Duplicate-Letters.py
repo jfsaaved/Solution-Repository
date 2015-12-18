bb12e352c359bc1a95c74b4d53114f50a4a95374
Julian Saavedra <julian.felipe.saavedra@gmail.com>
2015-12-17 21:45:30 -0500

    Added Remove-Duplicate-Letters

--- LeetCode/Medium/Python/Remove-Duplicate-Letters.py
+++ LeetCode/Medium/Python/Remove-Duplicate-Letters.py
@@ -9,64 +9,16 @@ class Solution(object):
         :rtype: str
         """
 
-        alpha_map = {}
-        index = 0
-        for c in s:
-            alpha_map.update({c: index})
-            index += 1
-
-        ceilings = sorted(alpha_map.values())
-        index = 0
-        lowest = 0
-        final_list = []
-
-        while index < len(ceilings):
-
-            to_add_list = list(s[lowest:ceilings[index] + 1])
-
-            smallest_item = {}
-            temp = lowest
-            for item in to_add_list:
-                if item not in final_list and item not in smallest_item:
-                    smallest_item.update({item: temp})
-                temp += 1
-
-            test = False
-            if smallest_item:
-                test = True
-
-            print("list: {} and map: {} status: {}".format(to_add_list, smallest_item, test))
-            priority_element = s[ceilings[index]]
-            if not smallest_item:
-                lowest = ceilings[index]
-            else:
-                add_this = priority_element
-                smaller_element = min(smallest_item)
-                if smaller_element not in final_list:
-                    if smaller_element < add_this:
-                        add_this = smaller_element
-
-                if isinstance(smallest_item.get(add_this), int):
-                    lowest = smallest_item.get(add_this) + 1
-                    final_list.append(add_this)
-                    print(add_this)
-                else:
-                    print("not able to add: {}".format(smallest_item.get(add_this)))
-
-            if priority_element in final_list:
-                index += 1
-
-        final_string = ""
-        for item in final_list:
-            final_string += item
-
+        final_string = ''
+        while s:
+            index = min(map(s.rindex, set(s)))
+            letter = min(s[:index+1])
+            final_string += letter
+            s = s[s.index(letter):].replace(letter, '')
         return final_string
 
 sol = Solution()
 print(sol.removeDuplicateLetters("caccabad"))
-print("------------------------------------")
 print(sol.removeDuplicateLetters("cbacdcbc"))
-print("------------------------------------")
 print(sol.removeDuplicateLetters("abcac"))
-print("------------------------------------")
 print(sol.removeDuplicateLetters("yioccqiorhtoslwlvfgzycahonecugtatbyphpuunwvaalcpndabyldkdtzfjlgwqk"))
--- README.md
+++ README.md
@@ -7,6 +7,6 @@ Problem | From | Difficulty | Language | Link
 [Range Sum Query - Immutable](https://leetcode.com/problems/range-sum-query-immutable/) | LeetCode | Easy | Python | [Solution](https://github.com/jfsaaved/Solution-Repository/blob/master/LeetCode/Easy/Python/Range-Sum-Query-Immutable.py)
 [Move Zeroes](https://leetcode.com/problems/move-zeroes/) | LeetCode | Easy | Python | [Solution](https://github.com/jfsaaved/Solution-Repository/blob/master/LeetCode/Easy/Python/Move-Zeroes.py)
 [Nim Game](https://leetcode.com/problems/nim-game/) | LeetCode | Easy | Python | [Solution](https://github.com/jfsaaved/Solution-Repository/blob/master/LeetCode/Easy/Python/Nim-Game.py)
-[Word Pattern](https://leetcode.com/problems/word-pattern/) | LeetCode | Easy | Python | [Solution](https://github.com/jfsaaved/Solution-Repository/blob/master/LeetCode/Easy/Python/Word%20Pattern.py)
+[Word Pattern](https://leetcode.com/problems/word-pattern/) | LeetCode | Easy | Python | [Solution](https://github.com/jfsaaved/Solution-Repository/blob/master/LeetCode/Easy/Python/Word-Pattern.py)
 [First Bad Version](https://leetcode.com/problems/first-bad-version/) | LeetCode | Easy | Python | [Solution](https://github.com/jfsaaved/Solution-Repository/blob/master/LeetCode/Easy/Python/First-Bad-Version.py)
 [Remove Duplicate Letters](https://leetcode.com/problems/remove-duplicate-letters/) | LeetCode | Medium | Python | [Solution](https://github.com/jfsaaved/Solution-Repository/blob/master/LeetCode/Medium/Python/Remove-Duplicate-Letters.py)
